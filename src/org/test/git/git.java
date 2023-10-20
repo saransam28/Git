0ace0a9f97284a3e331215f2e6e57e885ae129f9
saransam28 <iamsaran28@gmail.com>
2023-10-20 15:06:44 +0530

    my new code for saran branch

--- src/org/test/git/git.java
+++ src/org/test/git/git.java
@@ -3,5 +3,7 @@ package org.test.git;
 public class git {
 public static void main(String[] args) {
 	System.out.println("hi this is my git project");
+	System.out.println("And this my first project ");
+	System.out.println("i have to learn more");
 }
 }
